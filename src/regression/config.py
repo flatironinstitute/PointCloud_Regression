e2bd4809f59e95d150f486c9cad0219147e9b448
EmperorAkashi <linchen4869@ucla.edu>
2023-03-04 15:50:51 -0500

    change default modelnet option

--- src/regression/config.py
+++ src/regression/config.py
@@ -17,7 +17,7 @@ class TrainingDataConfig:
     #option for model net
     category: List[str] = dataclasses.field(default_factory=lambda:["airplane"])
     sigma: float = 0.01
-    model_net: bool = True
+    model_net: bool = False
 
 
 @dataclasses.dataclass
