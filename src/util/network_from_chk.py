298c3b367c74ce5a0212e8e1ed8aaf9ebf32f12d
EmperorAkashi <linchen4869@ucla.edu>
2023-06-09 17:51:06 -0400

    update the ckpt loading with options

--- src/util/network_from_chk.py
+++ src/util/network_from_chk.py
@@ -57,15 +57,19 @@ def get_svd_quat(cloud: torch.Tensor) -> torch.Tensor:
 
     return quat_list
 
-def get_batch_angle_diff(cloud: torch.Tensor, pred_quat_adj: torch.Tensor, pred_quat_chr: torch.Tensor, 
-                pred_quat_amt: torch.Tensor, true_quat: torch.Tensor):
+def get_batch_angle_diff(cloud: torch.Tensor, true_quat: torch.Tensor,
+                        pred_quat_adj: torch.Tensor = None, 
+                        pred_quat_chr: torch.Tensor = None, 
+                        pred_quat_amt: torch.Tensor = None,
+                        pred_quat_six: torch.Tensor = None):
     #the input can be wrap up as a dict for different predicted quat
     svd_quat = get_svd_quat(cloud)
     svd_list = M.quat_angle_diff(svd_quat, true_quat, reduce=False)
-    net_list_adj = M.quat_angle_diff(pred_quat_adj, true_quat, reduce=False)
-    net_list_chr = M.quat_angle_diff(pred_quat_chr, true_quat, reduce=False)
-    net_list_amt = M.quat_angle_diff(pred_quat_amt, true_quat, reduce=False)
-    return svd_list, net_list_adj, net_list_chr, net_list_amt#4 torch.Tensors
+    net_list_adj = M.quat_angle_diff(pred_quat_adj, true_quat, reduce=False) if pred_quat_adj is not None else None
+    net_list_chr = M.quat_angle_diff(pred_quat_chr, true_quat, reduce=False) if pred_quat_chr is not None else None
+    net_list_amt = M.quat_angle_diff(pred_quat_amt, true_quat, reduce=False) if pred_quat_amt is not None else None
+    net_list_six = M.quat_angle_diff(pred_quat_six, true_quat, reduce=False) if pred_quat_six is not None else None
+    return svd_list, net_list_adj, net_list_chr, net_list_amt, net_list_six#4 torch.Tensors
 
 def generate_fig(svd_list:torch.Tensor, net_list_adj:torch.Tensor, net_list_chr:torch.Tensor, net_list_amt:torch.Tensor):
     #need wrap up more lines; need add title and axes
@@ -88,12 +92,17 @@ def generate_fig(svd_list:torch.Tensor, net_list_adj:torch.Tensor, net_list_chr:
 
     return fig
 
-def generate_dict(svd_list:torch.Tensor, net_list_adj:torch.Tensor, net_list_chr:torch.Tensor, net_list_amt:torch.Tensor):
+def generate_dict(svd_list:torch.Tensor, 
+                net_list_adj: torch.Tensor = None, 
+                net_list_chr: torch.Tensor = None, 
+                net_list_amt: torch.Tensor = None,
+                net_list_six: torch.Tensor = None):
     save_data = {}
     save_data["svd_list"] = svd_list.detach().numpy()
-    save_data["net_list_adj"] = net_list_adj.detach().numpy()
-    save_data["net_list_chr"] = net_list_chr.detach().numpy()
-    save_data["net_list_amt"] = net_list_amt.detach().numpy()
+    save_data["net_list_adj"] = net_list_adj.detach().numpy() if net_list_adj is not None else []
+    save_data["net_list_chr"] = net_list_chr.detach().numpy() if net_list_chr is not None else []
+    save_data["net_list_amt"] = net_list_amt.detach().numpy() if net_list_amt is not None else []
+    save_data["net_list_six"] = net_list_six.detach().numpy() if net_list_six is not None else []
     return save_data
 
 def main():
@@ -102,33 +111,38 @@ def main():
     parser.add_argument('chkpt_path_adj', type = str, help = 'path of lightning check point with adjugate training')
     parser.add_argument('chkpt_path_chr', type = str, help = 'path of lightning check point with chordal training')
     parser.add_argument('chkpt_path_amt', type = str, help = 'path of lightning check point with a-matrix training')
+    parser.add_argument('chkpt_path_six', type = str, help = 'path of lightning check point with 6D training')
     parser.add_argument('output_path', type = str, help = 'path of save figures and text')
 
     args = parser.parse_args()
 
+    #we set the path as string None, if the path is not available
     check_point_adj = args.chkpt_path_adj
     check_point_chr = args.chkpt_path_chr
     check_point_amt = args.chkpt_path_amt
+    check_point_six = args.chkpt_path_six
 
     cloud_data  = args.npz_path
     save_path = args.output_path
 
-    pointnet_model_adj = read_check_point(check_point_adj)
-    pointnet_model_chr = read_check_point(check_point_chr)
-    pointnet_model_amt = read_check_point(check_point_amt)
+    pointnet_model_adj = read_check_point(check_point_adj) if check_point_adj != "None" else None
+    pointnet_model_chr = read_check_point(check_point_chr) if check_point_chr != "None" else None
+    pointnet_model_amt = read_check_point(check_point_amt) if check_point_amt != "None" else None
+    pointnet_model_six = read_check_point(check_point_six) if check_point_six != "None" else None
 
     cloud, true_quat = load_npz(cloud_data)
-    pred_quat_adj = forward_loaded_model(pointnet_model_adj, cloud, "adjugate")
-    pred_quat_chr = forward_loaded_model(pointnet_model_chr, cloud, "chordal")
-    pred_quat_amt = forward_loaded_model(pointnet_model_amt, cloud, "a-matrix")
+    pred_quat_adj = forward_loaded_model(pointnet_model_adj, cloud, "adjugate") if pointnet_model_adj != None else None
+    pred_quat_chr = forward_loaded_model(pointnet_model_chr, cloud, "chordal") if pointnet_model_chr != None else None
+    pred_quat_amt = forward_loaded_model(pointnet_model_amt, cloud, "a-matrix") if pointnet_model_amt != None else None
+    pred_quat_six = forward_loaded_model(pointnet_model_six, cloud, "six-d") if pointnet_model_six != None else None
 
-    list_svd_diff, list_adj_diff, list_chr_diff, list_amt_diff = \
-                get_batch_angle_diff(cloud, pred_quat_adj, pred_quat_chr, pred_quat_amt, true_quat)
-    delta_q_fig = generate_fig(list_svd_diff, list_adj_diff, list_chr_diff, list_amt_diff)
-    delta_q_fig.savefig(save_path+"/angle_diff.png")
+    list_svd_diff, list_adj_diff, list_chr_diff, list_amt_diff, list_six_diff = \
+                get_batch_angle_diff(cloud, true_quat, pred_quat_adj, pred_quat_chr, pred_quat_amt, pred_quat_six)
+    #delta_q_fig = generate_fig(list_svd_diff, list_adj_diff, list_chr_diff, list_amt_diff)
+    #delta_q_fig.savefig(save_path+"/angle_diff.png")
 
-    delta_q_dict = generate_dict(list_svd_diff, list_adj_diff, list_chr_diff, list_amt_diff)
-    np.savez(save_path+"/angle_diff.npz", **delta_q_dict)
+    delta_q_dict = generate_dict(list_svd_diff, list_adj_diff, list_chr_diff, list_amt_diff, list_six_diff)
+    np.savez(save_path+"/angle_diff_one_src.npz", **delta_q_dict)
 
 if __name__ == '__main__':
     main()
