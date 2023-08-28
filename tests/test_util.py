90f2d1c2481836ff124a6349a8feccfbd0361bc4
EmperorAkashi <linchen4869@ucla.edu>
2023-08-28 13:29:04 -0700

    add comments on test

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,6 +5,9 @@ from scipy.spatial.transform import Rotation as R
 import simulator.quat_util as Q
 
 def test_quat2rot():
+    """test fn to verify the Scipy uses JPL convention of quaternion
+    in simulations, one should stick with a fixed convention
+    """
     # generate a random quat
     quat_ = Q.generate_random_quat()
     r_scipy = R.from_quat(quat_)
