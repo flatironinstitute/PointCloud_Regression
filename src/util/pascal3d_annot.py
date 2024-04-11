70fb094df9f1df58b4f2da1abcb76171813fa4e7
EmperorAkashi <linchen4869@ucla.edu>
2024-04-11 18:17:42 -0400

    finish anno, start image cropping

--- src/util/pascal3d_annot.py
+++ src/util/pascal3d_annot.py
@@ -1,22 +1,21 @@
 import numpy as np
 import scipy.io
 
-from typing import Dict
+from typing import Dict, Any
 
     
-def read_annotaions(ann_file:str) -> Dict:
+def read_annotaions(ann_file:str) -> Dict[str, Any]:
     """@args:
     segmented: indicate whether there is a semantic map available
-    objects: wrap up all pose related items 
+    objects: wrap up all pose related items and bounding box 
     """
     ann_data = scipy.io.loadmat(ann_file)
 
     img_file = ann_data['record']['filename'][0][0][0]
-
     segmented = ann_data['record']['segmented'][0][0][0]
+    obj = ann_data['record']['objects'][0][0][0]
 
-
-    obj = ann_data['record']['objects'][0][0][0]:
+    category = obj['class'][0]
     if not obj['viewpoint']:
         return {}
     elif 'distance' not in obj['viewpoint'].dtype.names:
@@ -35,12 +34,23 @@ def read_annotaions(ann_file:str) -> Dict:
                             viewpoint['py'][0][0][0][0]])
     curr_dict = {
             'image_name': img_file,
-            'azimuth': azimuth,
-            'elevation': elevation,
-            'distance': distance,
-            'focal': focal,
-            'theta': theta,
-            'principal': principal
+            'category': category, 
+            'bbox': obj['bbox'][0],
+            'view':{
+                'azimuth': azimuth,
+                'elevation': elevation,
+                'distance': distance,
+                'focal': focal,
+                'theta': theta
+            },
+            'intrinsic':{
+                'focal': focal,
+                'principal': principal
+            }
         }
     
     return curr_dict
+
+class ROILoader():
+    """@brief: base class to do image preprocess and augmentation
+    """
